ac5da10bd5696a66f41c7b1ba39f9218a920c1d7
Laxmikant Nishad <laxmikantn07@gmail.com>
2021-09-11 14:00:47 +0500

    Second Commit

--- public/index.js
+++ public/index.js
@@ -1,55 +1,78 @@
+// jshint esversion:6
 import * as THREE from './threejs/three.module.js';
 import {STLLoader} from './threejs/STLLoader.js';
 import {OrbitControls} from './threejs/OrbitControls.js';
+const scene = new THREE.Scene()
+scene.add(new THREE.AxesHelper(5))
 
-let scene, camera, renderer, object;
+const light = new THREE.SpotLight()
+light.position.set(20, 20, 20)
+scene.add(light)
 
-function init(){
-    scene = new THREE.Scene();
-    scene.background = new THREE.Color(0x2a003b);
+let light2 = new THREE.DirectionalLight(0xffffff);
+light.position.set(0,0,10);
+scene.add(light2);
 
-    camera = new THREE.PerspectiveCamera(
-        75,
-        window.innerWidth / window.innerHeight,
-        0.1,
-        10000
-    );
-    camera.position.z = 10;
+let light3 = new THREE.DirectionalLight(0xffffff);
+light2.position.set(0,0,-10);
+scene.add(light3);
 
-    renderer = new THREE.WebGLRenderer();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    document.body.appendChild(renderer.domElement);
+const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+)
+camera.position.z = 10
 
-    scene.add(object);
+const renderer = new THREE.WebGLRenderer()
+renderer.outputEncoding = THREE.sRGBEncoding
+renderer.setSize(window.innerWidth, window.innerHeight)
+document.body.appendChild(renderer.domElement)
 
-    let control = new OrbitControls(camera, renderer.domElement);
+const controls = new OrbitControls(camera, renderer.domElement)
+controls.enableDamping = true
 
-    let light = new THREE.DirectionalLight(0xffffff);
-    light.position.set(0,0,10);
-    scene.add(light);
+const material = new THREE.MeshPhysicalMaterial({
+    color: 0xBEC4B5,
+    roughness: 0.1,
+})
 
-    let light2 = new THREE.DirectionalLight(0xffffff);
-    light2.position.set(0,0,-10);
-    scene.add(light2);
+const loader = new STLLoader()
+loader.load(
+    '3dmodels/FWMWK-lowerjaw.stl',
+    function (geometry) {
+        const mesh = new THREE.Mesh(geometry, material)
+        mesh.scale.set(0.1, 0.1, 0.1);
+        mesh.position.set(0,-5,0);
+        mesh.rotation.x = -Math.PI/2;
+        scene.add(mesh)
+    },
+    (xhr) => {
+        console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
+    },
+    (error) => {
+        console.log(error)
+    }
+)
 
-    animate();
+window.addEventListener('resize', onWindowResize, false)
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight
+    camera.updateProjectionMatrix()
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    render()
+}
+function animate() {
+    requestAnimationFrame(animate)
+
+    controls.update()
+
+    render()
 }
 
-function animate(){
-    requestAnimationFrame(animate);
-    renderer.render(scene, camera);
+function render() {
+    renderer.render(scene, camera)
 }
 
-let loader = new STLLoader();
-loader.load('/3dmodels/FWMWK-lowerjaw.stl', (model)=>{
-    object = new THREE.Mesh(
-        model,
-        new THREE.MeshLambertMaterial({color: 0xBEC4B5})
-    );
-    object.scale.set(0.1, 0.1, 0.1);
-    object.position.set(0,-5,0);
-    object.rotation.x = -Math.PI/2;
-    init();
-});
-
-
+animate()
